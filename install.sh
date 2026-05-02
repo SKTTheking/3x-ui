6d82e1ce78ddb57729f7ae9156a2a870f2fc208c
SKTTheking <77275129+SKTTheking@users.noreply.github.com>
2026-05-02 10:55:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -820,7 +820,7 @@ install_x-ui() {
             exit 1
         fi
     fi
-    curl -4fLRo /usr/bin/x-ui-temp https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
+    curl -4fLRo /usr/bin/x-ui-temp https://raw.githubusercontent.com/SKTTheking/3x-ui/main/x-ui.sh
     if [[ $? -ne 0 ]]; then
         echo -e "${red}Failed to download x-ui.sh${plain}"
         exit 1
